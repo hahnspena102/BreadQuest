b5f0e9e245a1bfe050260a7803b2c058a222b6ff
Camila Yeremin <yereminsasha@gmail.com>
2025-02-16 17:58:52 -0800

    adding paralax final

--- Assets/PARALLAX/ParallaxScript.cs
+++ Assets/PARALLAX/ParallaxScript.cs
@@ -3,20 +3,25 @@ using UnityEngine;
 public class parallax : MonoBehaviour
 
 {
+    [SerializeField] private Rigidbody2D rb;
+
     Material mat;
     float distance;
 
     [Range(0f, 0.5f)]
-    public float speed=0.2f;
+    public float speed= 0f; 
     void Start()
         
     {
+        
         mat= GetComponent<Renderer>().material;
         
     }
 
     void Update()
     {
+        speed = rb.linearVelocity.x * 0.0005f;
+        Debug.Log(speed);
         distance +=Time.deltaTime* speed;
         mat.SetTextureOffset("_MainTex", Vector2.right * distance);
 
